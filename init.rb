f4a628332af6f5b3fb2f77c8171897827baccebe
Takashi Oguma <bear.mini@gmail.com>
2010-11-22 21:17:44 +0900

    Supported 'label' option.
    Now you can use the following formats:
    1) unc(\\server-name\dir\to\file)
    2) unc(\\server-name\dir\to\file, Label of the file)
    
    The first one is the ordinary format.
    The second one is a new format with specifying a label of the file.

--- init.rb
+++ init.rb
@@ -14,19 +14,47 @@ class UncHelper
     return "" if !is_unc?(str)
     str.gsub(@head, "file://///").gsub(/\\/, "/")
   end
+
+  def trim(str)
+    return str.strip unless str == nil
+    return nil
+  end
+
+  def parse_args(args)
+    unc = trim(args[0])
+    label = trim(args[1]) || unc
+
+    return unc, label
+  end
+
+  def get_tag(args)
+    return "(No parameters are specified. A UNC path is needed at least.)" if args.empty?
+    unc, label = parse_args(args)
+
+    return <<TEMPLATE
+<a href=\"#{unc_to_file_proto(unc)}\">#{label}</a>
+TEMPLATE
+  end
+
 end
 
 Redmine::Plugin.register :redmine_wiki_unc do
   name 'Redmine Wiki Unc plugin'
   author 'Takashi Oguma'
   description 'This is a plugin for macro of Redmine Wiki'
-  version '0.0.2'
+  version '0.0.3'
 
   Redmine::WikiFormatting::Macros.register do
-    desc "make a link to UNC path.\n"
+    desc <<DESC
+Makes a link to UNC path.
+How to use:
+1) without a label: {{unc(\\\\server\\path\\to\\file)}}
+2) with a label:    {{unc(\\\\server\\path\\to\\file, My Secret Document)}}
+DESC
+
     macro :unc do |obj, args|
       h = UncHelper.new
-      "<a href=\"#{h.unc_to_file_proto(args[0])}\">#{args}</a>"
+      h.get_tag(args)
     end
   end
 end
